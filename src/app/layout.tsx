be166bafc6bc15543013a6ba0122456b6a7b9008
Pranav Kr <guytesting85@gmail.com>
2025-02-15 21:02:41 +0530

    trial upload
    vis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import Navbar from "@/components/Navbar";
+import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Toaster } from "react-hot-toast";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -24,12 +31,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ConvexClerkProvider>
+
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+
+            <SignedIn>
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="px-4 sm:px-6 lg:px-8 ">{children}</main>
+            </div>
+            </SignedIn>
+
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </html>
+
+    </ConvexClerkProvider>
   );
 }
